f69f2472ac5ab5ecf18c1945b8fd9e022eac4e18
Hem Raj Rana <hemraj.rana@hoolime.co>
2022-01-21 00:36:43 +0530

    tailwind working now

--- src/Start.svelte
+++ src/Start.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
 </script>
 
-<main>
-  <div class="container text-center">Spsspsps....</div>
+<main class="w-full h-full">
+  <div class="text-center text-white">Spsspsps....</div>
 </main>
 
 <style lang="postcss" global>
   @tailwind utilities;
   @tailwind components;
   @tailwind base;
+
   :root {
     font-size: 16px;
     font-family: monospace;
     box-sizing: border-box;
+    background-color: #1b1a1a;
   }
 </style>
